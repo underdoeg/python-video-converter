d26a0a89408e4e87fa7a2e3da80d48915ae6fbfb
Sebastien de Melo <sebastien.de-melo@ubicast.eu>
2015-10-14 10:21:51 +0200

    bump version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class DocCommand(Command):
 
 setup(
     name='VideoConverter',
-    version='1.1.1',
+    version='1.2.0',
     description='Video Converter library',
     url='https://github.com/senko/python-video-converter/',
 
