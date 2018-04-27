dd2433091da850accf7f8f27b8e9d18f9dab746c
Anthony Violo <anthony.violo@ubicast.eu>
2018-04-27 17:07:34 +0200

    Upper buffers size stored

--- converter/ffmpeg.py
+++ converter/ffmpeg.py
@@ -459,7 +459,7 @@ class FFMpeg(object):
         if preopts:
             cmds.extend(preopts)
         cmds.extend(['-i', infile])
-        cmds.extend(['-max_muxing_queue_size', '300'])
+        cmds.extend(['-max_muxing_queue_size', '500'])
         cmds.extend(opts)
         cmds.extend(['-y', outfile])
 
